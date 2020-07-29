9de4f9c907308b581c2418b2f53b83b1406d1d4c
Kev1n <c1048271926@outlook.com>
2020-07-29 18:39:30 +0800

    Add files via upload

--- 1276.cpp
+++ 1276.cpp
@@ -3,50 +3,52 @@
 using namespace std;
 struct warriors{
     int num;
-    int count;
     bool is = true;
 };
 int main(){
     int n;
     cin>>n;
     while(n--){
-        vector<warriors> w;
         int k;
         cin>>k;
+        vector<warriors> w;
         for(int i = 1;i <= k;i++){
             warriors temp;
             temp.num = i;
             w.push_back(temp);
         }
-        while(w.size() > 3){
+        while(1){
             for(int i = 0;i < w.size();i++){
-                if(i % 2 == 0)
+                if((i + 1) % 2 == 0)
                 w[i].is = false;
             }
-            for(int i = 0;i < w.size();i++){
-                if(!w[i].is)
-                w.erase(w.cbegin() + i + 1);
-                cout<<w[i].num<<endl;
+            vector<warriors>::iterator iter = w.begin();
+            while(iter != w.end()){
+                if((*iter).is == false)
+                    iter = w.erase(iter);
+                else ++iter;
             }
-            cout<<"**2"<<endl;
-            cout<<w.size()<<"*"<<endl;
             if(w.size() <= 3)
             break;
             for(int i = 0;i < w.size();i++){
-                if(i % 3 == 0)
+                if((i + 1) % 3 == 0)
                 w[i].is = false;
             }
-            for(int i = 0;i < w.size();i++){
-                if(!w[i].is)
-                w.erase(w.cbegin() + i + 1);
-                cout<<w[i].num<<endl;
+            iter = w.begin();
+            while(iter != w.end()){
+                if((*iter).is == false)
+                    iter = w.erase(iter);
+                else ++iter;
             }
-            cout<<"**3"<<endl;
+            if(w.size() <= 3)
+            break;
         }
-        cout<<"***"<<endl;
-        cout<<w.size()<<endl;
-        for(int i = 0;i < w.size();i++)
-        cout<<w[i].num<<endl;
+        for(auto iter = w.begin();iter != w.end();++iter){
+            if(iter == w.begin())
+                cout<<(*iter).num;
+            else cout<<" "<<(*iter).num;
+        }
+        cout<<endl;
     }
     return 0;
 }
